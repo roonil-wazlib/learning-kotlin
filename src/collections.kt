6380a3ff70a1a550cfa0787da65372fe55cf0981
roonil-wazlib <elfhogan@gmail.com>
2019-11-19 23:08:18 +1300

    Working through collections concepts

---
+++ src/collections.kt
@@ -0,0 +1,30 @@
+fun main(){
+    /* arrays are of fixed length, although they are mutable.
+        Therefore, usually lists are used.
+     */
+
+    val strings = listOf("Alice", "Bob", "Eve")
+    val map = mapOf("a" to 1, "b" to 2, "c" to 3)
+    val set = setOf("foo", "bar", "baz")
+
+    /*collections are both fixed length and immutable unless declared otherwise.
+    Declaring mutable collections is done like this:
+     */
+
+    val stringsAgain = mutableListOf("Alice", "Bob", "Eve")
+
+    println(stringsAgain.size) //.size for collections except for strings, which are .length
+
+    /* if declaring empty collections, the type must be specified */
+
+    val noInts: List<Int> = mutableListOf() //this was an example in documentation but what does it do?
+    val noStrings = listOf<String>()
+    val emptyMap = mapOf<String, Int>()
+
+    val noInts2 = mutableListOf<Int>()
+
+    println(noInts)
+    noInts2.add(1)
+    println(noInts2)
+
+}
